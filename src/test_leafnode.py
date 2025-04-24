0bb60e40edc098177ed470693bcb432ec5b52dc3
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2025-04-24 18:08:43 +0200

    implemented LeafNode

---
+++ src/test_leafnode.py
@@ -0,0 +1,15 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_raises_value_exception(self):
+        with self.assertRaises(ValueError) as context:
+            LeafNode("p").to_html()
+        self.assertEqual(str(context.exception), "All leaf nodes must have a value.")
